ae90ed2457bf42b337ed6112188909574d703b11
hwabis <kennysun23@gmail.com>
2024-11-17 09:57:26 -0500

    🧪

--- SooshEgoServer/SooshEgoServer.Tests/GamesManagerTests.cs
+++ SooshEgoServer/SooshEgoServer.Tests/GamesManagerTests.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace SooshEgoServer.Tests
 {
-    public class GameManagerTests
+    public class GamesManagerTests
     {
         [Fact]
         public void TestCreatedGameIdsAreUnique()
@@ -20,23 +20,43 @@ namespace SooshEgoServer.Tests
         }
 
         [Fact]
-        public void TestAddPlayers()
+        public void TestAddAndGetPlayers()
         {
             Mock<ILogger<GamesManager>> mockLogger = new();
             GamesManager gamesManager = new(mockLogger.Object);
 
             GameId gameId = gamesManager.CreateGame();
 
+            Assert.True(gamesManager.GetPlayerNames(gameId).success
+                && gamesManager.GetPlayerNames(gameId).playerNames.Count() == 0);
+            
             Assert.False(gamesManager.AddPlayerToGame(gameId, new PlayerName("")).success);
+            Assert.True(gamesManager.GetPlayerNames(gameId).success
+                && gamesManager.GetPlayerNames(gameId).playerNames.Count() == 0);
+
             Assert.True(gamesManager.AddPlayerToGame(gameId, new PlayerName("1")).success);
+            Assert.True(gamesManager.GetPlayerNames(gameId).success
+                && gamesManager.GetPlayerNames(gameId).playerNames.Count() == 1
+                && gamesManager.GetPlayerNames(gameId).playerNames.First()?.Value == "1");
+
             Assert.False(gamesManager.AddPlayerToGame(gameId, new PlayerName("1")).success);
+
             Assert.True(gamesManager.AddPlayerToGame(gameId, new PlayerName("2")).success);
             Assert.True(gamesManager.AddPlayerToGame(gameId, new PlayerName("3")).success);
             Assert.True(gamesManager.AddPlayerToGame(gameId, new PlayerName("4")).success);
             Assert.True(gamesManager.AddPlayerToGame(gameId, new PlayerName("5")).success);
+
             Assert.False(gamesManager.AddPlayerToGame(gameId, new PlayerName("6")).success);
+        }
+
+        [Fact]
+        public void TestAddAndGetPlayersNonExistingGame()
+        {
+            Mock<ILogger<GamesManager>> mockLogger = new();
+            GamesManager gamesManager = new(mockLogger.Object);
 
             Assert.False(gamesManager.AddPlayerToGame(new GameId("mystery lobby"), new PlayerName("mr. lost")).success);
+            Assert.False(gamesManager.GetPlayerNames(new GameId("mystery lobby")).success);
         }
     }
 }
