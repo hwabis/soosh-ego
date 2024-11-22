6bb52db0fed194b0c33701bfdf68e8d442b4b9c9
hwabis <kennysun23@gmail.com>
2024-11-22 13:27:41 -0500

    delete template svgs, add adding player

--- SooshEgoClient/src/components/GameLobby.tsx
+++ SooshEgoClient/src/components/GameLobby.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { createGame } from '../services/ApiService';
+import { createGame, addPlayer } from '../services/ApiService';
 
 const GameLobby = () => {
   const [playerName, setPlayerName] = useState('');
-  // todo entered game id
+  const [enteredGameId, setEnteredGameId] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const navigate = useNavigate();
@@ -19,18 +19,27 @@ const GameLobby = () => {
     }
   }
 
+  const handleJoinGame = async () => {
+    try {
+      await addPlayer(enteredGameId, playerName);
+      navigate(`/play/${enteredGameId}?playerName=${encodeURIComponent(playerName)}`);
+    } catch (error) {
+      setErrorMessage(String(error));
+    }
+  }
+
   return (
     <div>
       <div>
         <label>Your name: </label>
-        <input type="text" value={playerName} onChange={(e) => setPlayerName(e.target.value)} />
+        <input type="text" value={playerName} onChange={e => setPlayerName(e.target.value)} />
       </div>
       <button onClick={handleCreateGame}>Create New Game</button>
       <div>
         <label>Game ID: </label>
-        <input type="text" />
+        <input type="text" value={enteredGameId} onChange={e => setEnteredGameId(e.target.value)} />
       </div>
-      <button>Join Game</button>
+      <button onClick={handleJoinGame}>Join Game</button>
       <div>{errorMessage}</div>
     </div>
   );
--- SooshEgoClient/src/services/ApiService.tsx
+++ SooshEgoClient/src/services/ApiService.tsx
@@ -2,11 +2,17 @@ export interface GameId {
     value: string;
 }
 
+export interface PlayerName {
+    value: string;
+}
+
 export const createGame = async (playerName: string): Promise<GameId> => {
     const response = await fetch(`${import.meta.env.VITE_SOOSH_EGO_API_URL}/api/gamelobby/create`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ Value: playerName })
+        body: JSON.stringify({
+            value: playerName
+        })
     });
 
     if (!response.ok) {
@@ -15,4 +21,20 @@ export const createGame = async (playerName: string): Promise<GameId> => {
     }
 
     return await response.json();
+};
+
+export const addPlayer = async (gameId: string, playerName: string): Promise<void> => {
+    const response = await fetch(`${import.meta.env.VITE_SOOSH_EGO_API_URL}/api/gamelobby/add-player`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            GameId: { value: gameId },
+            PlayerName: { value: playerName }
+        })
+    });
+
+    if (!response.ok) {
+        const errorData = await response.text();
+        throw new Error(errorData);
+    }
 };
