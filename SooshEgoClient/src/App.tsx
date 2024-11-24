fdf77544847549d8e68a5e6ad5ac015cf39db14f
hwabis <kennysun23@gmail.com>
2024-11-24 13:02:41 -0500

    i still dont fully understand tailwind but this is good enough for now

--- SooshEgoClient/src/App.tsx
+++ SooshEgoClient/src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import GameLobby from './components/GameLobby';
+import GameLobbyScreen from './components/GameLobbyScreen';
 import PlayScreen from './components/PlayScreen';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<GameLobby />} />
+        <Route path="/" element={<GameLobbyScreen />} />
         <Route path="/play" element={<PlayScreen />} />
       </Routes>
     </Router>
---
+++ SooshEgoClient/src/components/GameLobbyScreen.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { createGame, addPlayer } from '../services/ApiService';
+
+const GameLobbyScreen = () => {
+  const [playerName, setPlayerName] = useState('');
+  const [enteredGameId, setEnteredGameId] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleCreateGame = async () => {
+    try {
+      const newGameId = await createGame(playerName);
+      navigate(`/play?gameId=${encodeURIComponent(newGameId.value)}&playerName=${encodeURIComponent(playerName)}`);
+    } catch (error) {
+      setErrorMessage(String(error));
+    }
+  }
+
+  const handleJoinGame = async () => {
+    try {
+      await addPlayer(enteredGameId, playerName);
+      navigate(`/play?gameId=${encodeURIComponent(enteredGameId)}&playerName=${encodeURIComponent(playerName)}`);
+    } catch (error) {
+      setErrorMessage(String(error));
+    }
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+      <div className="p-6 bg-red-900 rounded space-y-4">
+        <label className="text-lg font-bold text-white">Sushi Go!</label>
+        <div className="space-y-2">
+          <label className="block font-medium text-white">Your name:</label>
+          <input
+            type="text"
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+            className="block w-full rounded p-2"
+          />
+          <button
+            onClick={handleCreateGame}
+            className="w-full text-white rounded p-2 bg-green-600  hover:bg-green-700"
+          >
+            Create New Game
+          </button>
+        </div>
+        <div className="space-y-2">
+          <label className="block font-medium text-white">Game ID:</label>
+          <input
+            type="text"
+            value={enteredGameId}
+            onChange={(e) => setEnteredGameId(e.target.value)}
+            className="block w-full rounded p-2"
+          />
+          <button
+            onClick={handleJoinGame}
+            className="w-full text-white rounded p-2 bg-green-600  hover:bg-green-700"
+          >
+            Join Game
+          </button>
+        </div>
+      </div>
+      <div className="text-red-900 font-medium h-6 m-4">{errorMessage}</div>
+    </div>
+  );
+}
+
+export default GameLobbyScreen;
